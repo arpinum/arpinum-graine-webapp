707450e65ac2eb790142e1b1fae9d18e9e36b1e7
Jean-Baptiste Dusseaut <jbdusseaut@arpinum.fr>
2014-12-23 09:23:53 +0100

    début travail sur la gestion des révisions

--- tasks/build.js
+++ tasks/build.js
@@ -1,8 +1,8 @@
 module.exports = function (grunt) {
     grunt.registerTask("build", function () {
-        grunt.task.run(["less", "js"]);
+        grunt.task.run(["clean", "less", "js"]);
         if (grunt.config.get("prod")) {
-            grunt.task.run("uglify");
+            grunt.task.run(["uglify", "rev"]);
         }
     });
 };
