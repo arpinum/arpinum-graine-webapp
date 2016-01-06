59ea48cefb0c827b92d2a6e9ff4fdaeb688e87a0
Michael Borde <michael.borde@ubimail.fr>
2016-01-06 23:20:49 +0100

    refactoring

--- bin/program.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-var Server = require('../server/server.js');
-
-new Server().start();
---
+++ server.js
@@ -0,0 +1,5 @@
+'use strict';
+
+var Server = require('./server/server.js');
+
+new Server().start();
